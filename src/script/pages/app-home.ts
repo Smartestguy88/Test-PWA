eb77c305d7ab52dcf738cec66a0405ca226c8a3b
Smartestguy88 <verysmartguy88@gmail.com>
2022-03-06 19:40:31 +1000

    Title Change

--- src/script/pages/app-home.ts
+++ src/script/pages/app-home.ts
@@ -8,7 +8,7 @@ import '@pwabuilder/pwainstall';
 export class AppHome extends LitElement {
   // For more information on using properties and state in lit
   // check out this link https://lit.dev/docs/components/properties/
-  @property() message = 'Welcome!';
+  @property() message = 'Welcome to Calebs Home Page!!';
 
   static get styles() {
     return css`
